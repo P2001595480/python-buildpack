27a1b40e5f846560e6f88ed7f2f6735cad7655ef
P2001595480 <59279039+P2001595480@users.noreply.github.com>
2020-02-21 14:07:21 +0100

    probier

--- src/python/supply/supply.go
+++ src/python/supply/supply.go
@@ -421,7 +421,7 @@ func (s *Supplier) InstallPipEnv() error {
 func (s *Supplier) InstallLibTGIF() error {
 
 	s.Log.Info("------> Installing ML libs")
-    cmd := exec.Command("python", "-m", "pip", "install", "Flask","waitress","WSGIserver","rest_framework","dj-database-url","django-toolbelt","djangorestframework","static3","django","dj-static","cfenv","gunicorn","psycopg2","pandas","numpy","pmdarima")
+    cmd := exec.Command("python", "-m", "pip", "install", "Flask","waitress","WSGIserver","dj-database-url","django-toolbelt","djangorestframework","static3","django","dj-static","cfenv","gunicorn","psycopg2","pandas","numpy","pmdarima")
 	output, err := cmd.CombinedOutput()
 
 	if err != nil {
